bad0670f286b6d54373daca0497ce6bc782ee136
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-07 16:48:21 +0100

    Update stencil config with react build

--- stencil-library/stencil.config.ts
+++ stencil-library/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'stencil-library',
@@ -17,8 +18,12 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    reactOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../packages/react/lib/components.ts',
+    }),
   ],
   testing: {
-    browserHeadless: "new",
+    browserHeadless: 'new',
   },
 };
