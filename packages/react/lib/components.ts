bc80212a330d531b98be76ace8a0861c1fb0716d
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-07 22:32:20 +0100

    switch to crownelements

--- packages/react/lib/components.ts
+++ packages/react/lib/components.ts
@@ -1,10 +1,10 @@
-import type { JSX } from "core";
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from "./react-component-lib";
+import { createReactComponent } from './react-component-lib';
 
-export const MyComponent = /*@__PURE__*/ createReactComponent<
-  JSX.MyComponent,
-  HTMLMyComponentElement
->("my-component");
+import type { JSX } from 'core';
+
+
+
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
