9107ec3cf4afdecd9feea085ddda832c20d1aa3b
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-10 21:18:21 +0100

    Add stencil config for vue output target

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,5 +1,6 @@
 import { Config } from '@stencil/core';
 import { reactOutputTarget } from '@stencil/react-output-target';
+import { vueOutputTarget } from '@stencil/vue-output-target';
 
 export const config: Config = {
   namespace: 'core',
@@ -21,6 +22,10 @@ export const config: Config = {
     reactOutputTarget({
       componentCorePackage: '@crownelements/core',
       proxiesFile: '../react/lib/components.ts',
+    }),
+    vueOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../vue/lib/components.ts',
     }),
   ],
   testing: {
