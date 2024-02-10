20f1e0e1e1b65d118ad23b95132c0d1176876928
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-10 19:44:45 +0100

    Build react library

---
+++ packages/react-library/lib/components.ts
@@ -0,0 +1,10 @@
+import type { JSX } from "@crownelements/core";
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from "./react-component-lib";
+
+export const MyComponent = /*@__PURE__*/ createReactComponent<
+  JSX.MyComponent,
+  HTMLMyComponentElement
+>("my-component");
