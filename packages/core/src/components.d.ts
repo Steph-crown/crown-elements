e764c02ce6eeb4a8ecc3760b8c5a470155b588e0
Steph Crown <emmanuelstephen024@gmail.com>
2024-02-15 22:22:42 +0100

    Implement filled variant of button

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -6,6 +6,48 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
+    /**
+     * CrownButton Component
+     * Represents a customizable button component.
+     */
+    interface CrownButton {
+        /**
+          * Specifies whether the button is focused when the page loads.
+          * @default false
+         */
+        "autofocus": boolean;
+        /**
+          * Additional CSS classes to apply to the button. Has to be Tailwind CSS classes.
+         */
+        "customClass": string;
+        /**
+          * The disabled state of the button.
+          * @default false
+         */
+        "disabled": boolean;
+        /**
+          * The name of the button, which is submitted with the form data.
+         */
+        "name": string;
+        /**
+          * The type of button. Possible values include "button", "submit", or "reset".
+          * @default "button"
+         */
+        "type": 'button' | 'submit' | 'reset';
+        /**
+          * The value associated with the button, which is submitted with the form data.
+         */
+        "value": string;
+        /**
+          * The variant of the button. Possible values include "filled", "outlined", "text", "elevated", or "tonal".
+         */
+        "variant": 'filled' | 'outlined' | 'text' | 'elevated' | 'tonal';
+        /**
+          * The width of the button. Possible values include "auto", "full", or specific CSS width (e.g., "200px"). "auto" sets the button width to the width of its content, "full" sets the button width to 100% of its container, and a specific CSS width sets the button width to the specified width.
+          * @default "auto"
+         */
+        "width": 'auto' | 'full' | string;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -22,6 +64,16 @@ export namespace Components {
     }
 }
 declare global {
+    /**
+     * CrownButton Component
+     * Represents a customizable button component.
+     */
+    interface HTMLCrownButtonElement extends Components.CrownButton, HTMLStencilElement {
+    }
+    var HTMLCrownButtonElement: {
+        prototype: HTMLCrownButtonElement;
+        new (): HTMLCrownButtonElement;
+    };
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
@@ -29,10 +81,53 @@ declare global {
         new (): HTMLMyComponentElement;
     };
     interface HTMLElementTagNameMap {
+        "crown-button": HTMLCrownButtonElement;
         "my-component": HTMLMyComponentElement;
     }
 }
 declare namespace LocalJSX {
+    /**
+     * CrownButton Component
+     * Represents a customizable button component.
+     */
+    interface CrownButton {
+        /**
+          * Specifies whether the button is focused when the page loads.
+          * @default false
+         */
+        "autofocus"?: boolean;
+        /**
+          * Additional CSS classes to apply to the button. Has to be Tailwind CSS classes.
+         */
+        "customClass"?: string;
+        /**
+          * The disabled state of the button.
+          * @default false
+         */
+        "disabled"?: boolean;
+        /**
+          * The name of the button, which is submitted with the form data.
+         */
+        "name"?: string;
+        /**
+          * The type of button. Possible values include "button", "submit", or "reset".
+          * @default "button"
+         */
+        "type"?: 'button' | 'submit' | 'reset';
+        /**
+          * The value associated with the button, which is submitted with the form data.
+         */
+        "value"?: string;
+        /**
+          * The variant of the button. Possible values include "filled", "outlined", "text", "elevated", or "tonal".
+         */
+        "variant"?: 'filled' | 'outlined' | 'text' | 'elevated' | 'tonal';
+        /**
+          * The width of the button. Possible values include "auto", "full", or specific CSS width (e.g., "200px"). "auto" sets the button width to the width of its content, "full" sets the button width to 100% of its container, and a specific CSS width sets the button width to the specified width.
+          * @default "auto"
+         */
+        "width"?: 'auto' | 'full' | string;
+    }
     interface MyComponent {
         /**
           * The first name
@@ -48,6 +143,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface IntrinsicElements {
+        "crown-button": CrownButton;
         "my-component": MyComponent;
     }
 }
@@ -55,6 +151,11 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
+            /**
+             * CrownButton Component
+             * Represents a customizable button component.
+             */
+            "crown-button": LocalJSX.CrownButton & JSXBase.HTMLAttributes<HTMLCrownButtonElement>;
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
         }
     }
